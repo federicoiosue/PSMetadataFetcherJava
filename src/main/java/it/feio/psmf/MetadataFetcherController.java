a5178e8db99a29dc962a11961cbd99433e73dbb8
Federico Iosue <federico.iosue@gmail.com>
2017-10-10 23:22:15 +0200

    Added some logging

--- src/main/java/it/feio/psmf/MetadataFetcherController.java
+++ src/main/java/it/feio/psmf/MetadataFetcherController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import it.feio.psmf.http.PlayStoreHttpClient;
 import it.feio.psmf.models.PlayStoreResult;
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MetadataFetcherController {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     static final String GREETING = "I just answer HELLO to check that everything works. I'm stupid, I know.!";
 
     @Autowired
@@ -26,6 +29,7 @@ public class MetadataFetcherController {
     @RequestMapping(value = "/", method = {RequestMethod.GET}, produces = "application/json")
     public String fetchMetadata(@RequestParam("app-package") String appPackage,
       @RequestParam(value = "lang", defaultValue = "en") String lang) throws IOException {
+        log.info("Requesting info for app '" + appPackage + "' using language '" + lang + "'");
         PlayStoreResult playStoreResult = playStoreHttpClient.get(appPackage, lang);
         return new ObjectMapper().writeValueAsString(playStoreResult);
     }
--- src/main/java/it/feio/psmf/http/PlayStoreHttpClient.java
+++ src/main/java/it/feio/psmf/http/PlayStoreHttpClient.java
@@ -5,6 +5,8 @@ import it.feio.psmf.models.PlayStoreResultAdapter;
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
@@ -14,8 +16,11 @@ public class PlayStoreHttpClient {
     final static String PLAY_STORE_URL = "https://play.google.com/store/apps/details";
     final static String CACHE_NAME = "appPackages";
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Cacheable(value = CACHE_NAME, key="{ #appPackage, #lang }")
     public PlayStoreResult get(String appPackage, String lang) throws IOException {
+        log.info("Uncached request for '" + appPackage + "' using language '" + lang + "'");
         Document doc = Jsoup.connect(buildAppUrl(appPackage, lang)).get();
         return PlayStoreResultAdapter.adapt(doc);
     }
